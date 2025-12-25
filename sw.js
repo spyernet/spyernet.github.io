26b11305408ebc201116b3f80f5da47aafad7156
spyernet <iammuzamil23.alt@gmail.com>
2025-12-25 15:49:12 +0000

    Save work (snapshot)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10366419
+    "zoneId": 10374758
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
